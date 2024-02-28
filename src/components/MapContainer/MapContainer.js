f7f58d86e65b8f18eaae31216f54575abb5f2e5c
Baptiste Higgs <Baptiste.Higgs@lendlease.com>
2024-02-28 18:12:02 +1100

    Draggable element

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -5,6 +5,8 @@ import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import FetchPolygonData from "../../utils/FetchPolygonData";
 import { ACCESS_TOKEN } from "../../constants";
 import { GetURLVariable } from "../../utils/urlVariableUtils";
+import Draggable from "react-draggable";
+import "./style.css";
 
 const Map = ReactMapboxGl({
   accessToken: GetURLVariable(ACCESS_TOKEN),
@@ -16,7 +18,6 @@ export const MapContainer = () => {
       return { lat: coords[1], lng: coords[0] };
     });
     FetchPolygonData(polygon);
-    console.log(window.location.search);
   };
 
   const onDrawUpdate = ({ features }) => {
@@ -25,6 +26,11 @@ export const MapContainer = () => {
 
   return (
     <div>
+      <Draggable className="draggable">
+        <div className="box">
+          <div>Move me around!</div>
+        </div>
+      </Draggable>
       <Map
         style="mapbox://styles/mapbox/streets-v9" // eslint-disable-line
         containerStyle={{
